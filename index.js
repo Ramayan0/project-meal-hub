00a17bf618db993f6d1adfff4af576ea891fa654
hamsa <hafieekhan@gmail.com>
2022-09-06 23:43:47 +0300

    Finally managed fiter Method

--- index.js
+++ index.js
@@ -1,88 +1,117 @@
+let arr = [];
+let select = document.querySelector("#select");
+
 document.addEventListener("DOMContentLoaded", function () {
-  // menu();
+  getData();
 });
-function renderOneMeal(menu) {
+
+function filterMenu() {
+  let option = select.options[select.selectedIndex].value;
+  let filterdData = arr.filter((menuItem) => {
+    if (option === "all") return menuItem;
+    return menuItem.category === option;
+  });
+  renderOneMeal(filterdData);
+  console.log(arr);
+}
+
+function renderOneMeal(menus) {
   // building the menu
   let card = document.createElement("table");
   card.className = "card";
-  // card.style.display = "none";
-  card.innerHTML = `
-    <div class="section-center">
-    <article class="menu-item">
-    <img src = "${menu.img}" alt="menu item" class="photo" >
-    <div class = "content">
-    <header>
-    <h4>${menu.title} </h4>
-    <h4 class="price"> $${menu.price} </h4>
-    </header>
-    <h4>${menu.category} </h4>
-    <p class "item-text"> ${menu.desc} </p>
+  // card.style.display = "none"
+  let data = "";
+  menus.forEach((menu) => {
+    data += `
+  <div class="section-center">
+  <article class="menu-item">
+  <img src = "${menu.img}" alt="menu item" class="photo" >
+  <div class = "content">
+  <header>
+  <h4>${menu.title} </h4>
+  <h4 class="price"> $${menu.price} </h4>
+  </header>
+  <h4>${menu.category} </h4>
+  <p class "item-text"> ${menu.desc} </p>
 
-    <div class = "button">
-    <button onclick="myFunction()"> Place Order</button>
-    </div>
-    </article>
-    </div>
-    `;
+  <div class = "button">
+  <button onclick="myFunction()"> Place Order</button>
+  </div>
+  </article>
+  </div>
+  `;
+  });
   // Add menu to DOM
-  document.querySelector("#menu-list").appendChild(card);
+  document.querySelector("#menu-list").innerHTML = data;
   likeBtn();
 }
-
-fetch("https://thawing-atoll-64866.herokuapp.com/menu")
-  .then((response) => response.json())
-  .then((menuData) => {
-    menuData.forEach((menu) => renderOneMeal(menu));
-
-    const filterBtn = document.querySelectorAll(".filter-btn");
-    filterBtn.forEach(function (btn) {
-      btn.addEventListener("click", function (e) {
-        const category = e.currentTarget.dataset.id;
-        const menuCategory = menuData.filter(function (menuItem) {
-          if (menuItem.category === category) {
-            return menuItem;
-          }
-        });
-        menuCategory.forEach((menu) => {
-          // renderOneMeal(menuMeal);
-          let card = document.createElement("table");
-          card.innerHTML = "";
-          card.className = "card";
-          card.innerHTML = `
-      <div class="section-center">
-      <article class="menu-item">
-      <img src = "${menu.img}" alt="menu item" class="photo" >
-      <div class = "content">
-      <header>
-      <h4>${menu.title} </h4>
-      <h4 class="price"> $${menu.price} </h4>
-      </header>
-      <h4>${menu.category} </h4>
-      <p class "item-text"> ${menu.desc} </p>
-      <div class = "likee">
-
-      <button class = "place-order" onclick="myFunction()"> Place Order</button>
-      <button class="btn btn-like">
-      <span class="btn-icon btn--icon-default">
-        <span class="fa fa-heart"></span>
-      </span>
-      <span class="btn-icon btn--icon-liked">
-        <span class="fa fa-heart"></span>
-      </span>
-    </button>
-    </div>
-      </article>
-      </div>
-      `;
-          // Add menu to DOM
-          document.querySelector("#menu-list").appendChild(card);
-        });
-        if (category === "all") {
-          return menuData;
-        }
+// let select = document.getElementById("select");
+// function handleSelect() {
+//   let selected = select.options[select.selectedIndex].value;
+// console.log(selected);
+// return selected;
+// }
+function getData() {
+  fetch("https://thawing-atoll-64866.herokuapp.com/menu")
+    .then((response) => response.json())
+    .then((menuData) => {
+      renderOneMeal(menuData);
+      menuData.forEach((menu) => {
+        arr.push(menu);
       });
     });
-  });
+}
+
+// const filterBtn = document.querySelectorAll(".filter-btn");
+// filterBtn.forEach(function (btn) {
+//   btn.addEventListener("click", function (e) {
+//     const category = e.currentTarget.dataset.id;
+//     const menuCategory = menuData.filter(function (menuItem) {
+//       return menuItem.category === category;
+//       //   if (menuItem.category === category) {
+//       //     return menuItem;
+//       //   }
+//     });
+// menuCategory.forEach((menu) => {
+//   // renderOneMeal(menuMeal);
+//   let card = document.createElement("table");
+//   card.innerHTML = "";
+//   card.className = "card";
+//   card.innerHTML = `
+//   <div class="section-center">
+//   <article class="menu-item">
+//   <img src = "${menu.img}" alt="menu item" class="photo" >
+//   <div class = "content">
+//   <header>
+//   <h4>${menu.title} </h4>
+//   <h4 class="price"> $${menu.price} </h4>
+//   </header>
+//   <h4>${menu.category} </h4>
+//   <p class "item-text"> ${menu.desc} </p>
+//   <div class = "likee">
+
+//   <button class = "place-order" onclick="myFunction()"> Place Order</button>
+//   <button class="btn btn-like">
+//   <span class="btn-icon btn--icon-default">
+//     <span class="fa fa-heart"></span>
+//   </span>
+//   <span class="btn-icon btn--icon-liked">
+//     <span class="fa fa-heart"></span>
+//   </span>
+// </button>
+// </div>
+//   </article>
+//   </div>
+//   `;
+//   // Add menu to DOM
+//   document.querySelector("#menu-list").appendChild(card);
+// });
+// if (category === "all") {
+//   return menuData;
+// }
+// });
+// });
+// });
 
 // alert
 function myFunction() {
